3e2f19dcea2276b4a20361bd756c0fe8a389bbdf
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2023-11-21 11:36:21 +0100

    is niet meer een healthscript is nu een pokemonstats script

---
+++ VerticalSlice/Assets/Scripts/PokemonStats.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonStats : MonoBehaviour
+{
+    public float Health;
+    public float Speed;
+    public float Damage;
+
+    void Start()
+    {
+        Debug.Log(Health);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
