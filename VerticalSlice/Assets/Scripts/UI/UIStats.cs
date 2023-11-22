8796c0e13c2463c11ddc8f0fb4f11532817798e2
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2023-11-22 12:08:21 +0100

    fix merge conflict

--- VerticalSlice/Assets/Scripts/UI/UIStats.cs
+++ VerticalSlice/Assets/Scripts/UI/UIStats.cs
@@ -1,18 +1,22 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIStats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image healthBar;
+    PokemonStats pokemonStats;
+
+    public int TotalHealth = 100;
+
+    public void Start()
+    {
+        
+    }
+    public void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
