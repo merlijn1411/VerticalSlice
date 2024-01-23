f49f19229465dbe645e234cc3705a05943153f19
keithko <25635@ma-web.nl>
2024-01-23 09:15:56 +0100

    ben klaar en ga mergen met develop branch

--- VerticalSlice/Assets/Scripts/UI/Dialogue.cs
+++ VerticalSlice/Assets/Scripts/UI/Dialogue.cs
@@ -5,76 +5,78 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    static public event Action OnFinishedDialogue;
+	static public event Action OnFinishedDialogue;
 
 
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    private float textspeed;
+	public TextMeshProUGUI textComponent;
+	public string[] lines;
+	private float textspeed;
 
-    private int index;
-    public GameObject Playing;
+	private int index;
+	public GameObject Playing;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Playing.SetActive(true);
-        textspeed = 2 * Time.deltaTime;
-        textComponent.text = string.Empty;
-        StartDialogue();
+	// Start is called before the first frame update
+	void Start()
+	{
+		Playing.SetActive(true);
+		textspeed = 2 * Time.deltaTime;
+		textComponent.text = string.Empty;
+		StartDialogue();
 
-        EndPanelAnimation.OnPanelAnimationEndEvent += PlayText;
-    }
+		EndPanelAnimation.OnPanelAnimationEndEvent += PlayText;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        PlayText();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		PlayText();
+	}
 
-    public void PlayText()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (textComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
-        }
-    }
+	public void PlayText()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (textComponent.text == lines[index])
+			{
+				NextLine();
+			}
+			else
+			{
+				StopAllCoroutines();
+				textComponent.text = lines[index];
+			}
+		}
+	}
 
-    void StartDialogue()
-    {
-        index = 0;
-        StartCoroutine(TypeLine());
-    }
+	void StartDialogue()
+	{
+		index = 0;
+		StartCoroutine(TypeLine());
+	}
 
-    IEnumerator TypeLine()
-    {
-        foreach (char C in lines[index].ToCharArray())
-        {
-            textComponent.text += C;
-            yield return new WaitForSeconds(textspeed);
-        }
-    }
+	IEnumerator TypeLine()
+	{
+		foreach (char C in lines[index].ToCharArray())
+		{
+			textComponent.text += C;
+			yield return new WaitForSeconds(textspeed);
+		}
 
-    void NextLine()
-    {
-        if (index < lines.Length - 1)
-        {
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        }
-        else
-        {
-            OnFinishedDialogue?.Invoke();
-        }
-    }
+	}
+
+	void NextLine()
+	{
+		if (index < lines.Length - 1)
+		{
+			index++;
+			textComponent.text = string.Empty;
+			StartCoroutine(TypeLine());
+		}
+		else
+		{
+			OnFinishedDialogue?.Invoke();
+			index = 0;
+		}
+	}
 }
