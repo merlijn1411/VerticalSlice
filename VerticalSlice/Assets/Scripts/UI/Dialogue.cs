372fb054ec94051d78521f5945b1d3a82a60bf9c
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-01-24 11:23:47 +0100

    duurt iets langer dan gedacht moet nu een andere manier vinden om de buttons en de text gelijk te hebben

--- VerticalSlice/Assets/Scripts/UI/Dialogue.cs
+++ VerticalSlice/Assets/Scripts/UI/Dialogue.cs
@@ -14,7 +14,8 @@ public class Dialogue : MonoBehaviour
 	private int index;
 	public GameObject Playing;
 
-	public Button Attackbtn1, Attackbtn2, Attackbtn3, Attackbtn4;
+	//public Button Attackbtn1, Attackbtn2, Attackbtn3, Attackbtn4;
+	public Button[] AttackBtn;
 
 	public string[] buttonTexts;
 
@@ -27,38 +28,34 @@ public class Dialogue : MonoBehaviour
 
 		EndPanelAnimation.OnPanelAnimationEndEvent += PlayText;
 
+		for (int i = 0; i < AttackBtn.Length; i++)
+		{
+			int Buttonvalue = i;
+			AttackBtn[i].onClick.AddListener(() => ButtonChooser(Buttonvalue));
+		}
 
 		index = 0;
-
-		Button btn1 = Attackbtn1.GetComponent<Button>();
-		btn1.onClick.AddListener(delegate { ButtonChooser(0); });
-
-		Button btn2 = Attackbtn2.GetComponent<Button>();
-		btn2.onClick.AddListener(delegate { ButtonChooser(1); });
-
-		Button btn3 = Attackbtn3.GetComponent<Button>();
-		btn3.onClick.AddListener(delegate { ButtonChooser(2); });
-
-		Button btn4 = Attackbtn4.GetComponent<Button>();
-		btn4.onClick.AddListener(delegate { ButtonChooser(3); });
 	}
 
 	public void PlayText()
 	{
-		if (textComponent.text == buttonTexts[index])
+		if (textComponent.text != buttonTexts[index])
 		{
-			NextLine();
+			//Debug.LogError(textComponent.text + " is not the same a the" + buttonTexts[index]);
+
 		}
 		else
 		{
 			StopAllCoroutines();
+			//StartCoroutine(TypeLine());
 			textComponent.text = buttonTexts[index];
+			index = 0;
 		}
 	}
 
-	IEnumerator TypeLine()
+	IEnumerator TypeLine(int buttonIndex)
 	{
-
+		Debug.Log(buttonIndex + "xeddw");
 		foreach (char C in buttonTexts[index].ToCharArray())
 		{
 			textComponent.text += C;
@@ -75,7 +72,7 @@ public class Dialogue : MonoBehaviour
 		if (index < buttonTexts.Length - 1)
 		{
 			index++;
-			StartCoroutine(TypeLine());
+			//StartCoroutine(TypeLine());
 		}
 		else
 		{
@@ -86,16 +83,10 @@ public class Dialogue : MonoBehaviour
 
 	public void ButtonChooser(int buttonIndex)
 	{
-		if (buttonIndex >= 0 && buttonIndex < buttonTexts.Length)
+
+		if (buttonIndex == 0)
 		{
-			StopAllCoroutines();
-			textComponent.text = string.Empty;
-			index = buttonIndex;
-			PlayText();
-		}
-		else
-		{
-			Debug.LogError("Invalid button index or buttonTexts array length insufficient.");
+			StartCoroutine(TypeLine(buttonIndex));
 		}
 	}
 }
