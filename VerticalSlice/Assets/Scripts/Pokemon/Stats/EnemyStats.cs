aa11bb511a3bb2421db57d5577971ce3aadbc741
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-01-30 22:01:11 +0100

    parts of the enemy dialogue

--- VerticalSlice/Assets/Scripts/Pokemon/Stats/EnemyStats.cs
+++ VerticalSlice/Assets/Scripts/Pokemon/Stats/EnemyStats.cs
@@ -32,11 +32,6 @@ public class EnemyStats : MonoBehaviour
 	{
 		healthbar.GetComponent<Healthbar>().CurrentHealth(CurrentHealth, healthbar.slider.value);
 
-		if (Input.GetKeyUp(KeyCode.Space))
-		{
-			PsyshockAnimtake();
-		}
-
 		if (CurrentHealth <= 0)
 		{
 			Invoke("Defeated", 4.5f);
@@ -45,7 +40,6 @@ public class EnemyStats : MonoBehaviour
 	public void PsyshockAnimtake()
 	{
 		PlayPsyshockTake.SetTrigger("Spheres");
-
 	}
 
 	private void Defeated()
